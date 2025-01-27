ae77b61b57fb9580589d3a3d7ef192eb60911fa3
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-01-28 00:52:45 +0100

    feat. add bonus part

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -5,27 +5,56 @@ import MovieCard from "../components/MovieCard";
 function MoviesList() {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        axios.get((`${backendUrl}/movies`)).then((resp) => {
+    function getMovie() {
+        const params = {};
+        if (search.length > 0) {
+            params.search = search
+        }
+
+        axios.get(`${backendUrl}/movies`, { params }).then((resp) => {
             setMovies(resp.data)
         });
+    };
+
+    useEffect(() => {
+        getMovie()
     }, []);
 
     return (
         <>
             <main className="container">
-                <section>
+                {/* Search input */}
+                <section className="my-4">
                     <h1>Ecco la lista dei film...</h1>
-                    <div className="row row-cols-1 row-cols-sm-2">
-                        {
-                            movies.map((curMovie, index) => (
-                                <div className="col" key={curMovie.id}>
-                                    <MovieCard movie={curMovie} />
-                                </div>
-                            ))
-                        }
+                    <div className="d-flex my-4">
+                        <input
+                            value={search}
+                            onChange={(event) => setSearch(event.target.value)} type="search"
+                            className="form-control"
+                            placeholder="Cerca i tuoi film preferiti..."
+                        />
+                        <button onClick={getMovie} className="btn btn-outline-secondary btn ms-2">Search</button>
                     </div>
+                </section>
+                {/* Movies List */}
+                <section>
+                    {
+                        movies.length > 0 ? (
+                            <div className="row row-cols-1 row-cols-sm-2">
+                                {
+                                    movies.map((curMovie, index) => (
+                                        <div className="col" key={curMovie.id}>
+                                            <MovieCard movie={curMovie} />
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        ) : (
+                            <div className="alert alert-secondary">Nessun film trovato, riprova!</div>
+                        )
+                    }
                 </section>
             </main>
         </>
