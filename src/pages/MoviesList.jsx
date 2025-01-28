32109013b7def1d5b85a003f96a31f64a1f0990e
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-01-28 19:35:48 +0100

    updete: add select for filter genres and year to Movies Page

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -4,13 +4,33 @@ import MovieCard from "../components/MovieCard";
 
 function MoviesList() {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const [movies, setMovies] = useState([]);
     const [search, setSearch] = useState("");
+    const [genre, setGenre] = useState("");
+    const [year, setYear] = useState("");
+
+    const genres = ["Action", "Crime", "Romance", "Science Fiction"];
+    const years = [];
+    const currentYear = new Date().getFullYear();
+    for (let year = 1970; year <= currentYear; year++) {
+        years.push(year);
+    };
+
 
     function getMovie() {
         const params = {};
+
         if (search.length > 0) {
             params.search = search
+        }
+
+        if (genre !== "") {
+            params.genre = genres[genre]
+        }
+
+        if (year !== "") {
+            params.release_year = years[year]
         }
 
         axios.get(`${backendUrl}/movies`, { params }).then((resp) => {
@@ -35,6 +55,30 @@ function MoviesList() {
                             className="form-control"
                             placeholder="Cerca i tuoi film preferiti..."
                         />
+                        <select
+                            class="form-select"
+                            aria-label="Default select example"
+                            value={genre}
+                            onChange={(event) => setGenre(event.target.value)}>
+                            <option selected>Genere...</option>
+                            {
+                                genres.map((curGenre, index) => (
+                                    <option key={index} value={index}>{curGenre}</option>
+                                ))
+                            }
+                        </select>
+                        <select
+                            class="form-select"
+                            aria-label="Default select example"
+                            value={year}
+                            onChange={(event) => setYear(event.target.value)}>
+                            <option selected>Yaer...</option>
+                            {
+                                years.map((curGenre, index) => (
+                                    <option value={index}>{curGenre}</option>
+                                ))
+                            }
+                        </select>
                         <button onClick={getMovie} className="btn btn-outline-secondary btn ms-2">Search</button>
                     </div>
                 </section>
